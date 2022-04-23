d444bde9e0bbfd756cf42d2dfa1e74a439df56ac
Dipal Bhavsar <bhavsar.priti@gmail.com>
2022-04-23 13:27:53 +0530

    Make production ready web site for all pages.
    Make production ready web site for all pages.
    Remove html extn

---
+++ rename.bat
@@ -0,0 +1,27 @@
+ren index.html index
+ren legal.html legal
+ren privacy-policy.html privacy-policy
+ren website-usage-terms-and-conditions.html website-usage-terms-and-conditions
+
+cd Products
+cd Portals 
+ren corehr.html corehr
+ren intranetportal.html intranetportal
+
+cd ..
+cd Templates
+
+ren template1.html template1
+ren template2.html template2
+ren template3.html template3
+ren templatedemo.html templatedemo
+ren webparts.html webparts
+
+cd ..
+cd ..
+
+cd Subscribe
+
+ren confirm.html confirm
+ren index.html index
+ren pendingconfirmation.html pendingconfirmation
