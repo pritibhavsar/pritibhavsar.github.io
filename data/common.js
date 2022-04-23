c2ed24c6db831fc7b63abd2cea30a308ea662cdf
Dipal Bhavsar <bhavsar.priti@gmail.com>
2022-04-23 17:46:29 +0530

    Test 1
    Test 1

--- data/common.js
+++ data/common.js
@@ -29,19 +29,19 @@ function getNavigationContent(idx) {
                     <div class="mega_menu_inner">
                         <ul class="dropdown-menu">
                             <li class="nav-item">
-                                <a href="~~~Products/Templates/templatedemo.html" class="nav-link">SharePoint Templates</a>
+                                <a href="~~~products/templates/templatedemo.html" class="nav-link">SharePoint Templates</a>
                                 <ul class="dropdown-menu">
-                                    <li class="nav-item"><a href="~~~Products/Templates/template1.html" class="nav-link">Demo 1</a></li>
-                                    <li class="nav-item"><a href="~~~Products/Templates/template2.html" class="nav-link">Demo 2</a></li>
-                                    <li class="nav-item"><a href="~~~Products/Templates/template3.html" class="nav-link">Demo 3</a></li>
-                                    <li class="nav-item"><a href="~~~Products/Templates/webparts.html" class="nav-link">Web Parts</a></li>
+                                    <li class="nav-item"><a href="~~~products/templates/template1.html" class="nav-link">Demo 1</a></li>
+                                    <li class="nav-item"><a href="~~~products/templates/template2.html" class="nav-link">Demo 2</a></li>
+                                    <li class="nav-item"><a href="~~~products/templates/template3.html" class="nav-link">Demo 3</a></li>
+                                    <li class="nav-item"><a href="~~~products/templates/webparts.html" class="nav-link">Web Parts</a></li>
                                 </ul>
                             </li>  
                             <li class="nav-item">
-                                <a href="~~~Products/Portals/index.html" class="nav-link" id ="Portals">Portals</a>
+                                <a href="~~~products/portals/index.html" class="nav-link" id ="Portals">Portals</a>
                                 <ul class="dropdown-menu">
-                                    <li class="nav-item"><a href="~~~Products/Portals/intranetportal.html" class="nav-link" id ="IntranetPortal">Intranet Portal</a></li>
-                                    <li class="nav-item"><a href="~~~Products/Portals/corehr.html" class="nav-link" id ="CoreHr">Core HR</a></li>
+                                    <li class="nav-item"><a href="~~~products/portals/intranetportal.html" class="nav-link" id ="IntranetPortal">Intranet Portal</a></li>
+                                    <li class="nav-item"><a href="~~~products/portals/corehr.html" class="nav-link" id ="CoreHr">Core HR</a></li>
                                     <li class="nav-item"><a href="#" class="nav-link">Time & Attendance</a></li>
                                     <li class="nav-item"><a href="#" class="nav-link">Recruiting</a></li>
                                     <li class="nav-item"><a href="#" class="nav-link">Performace</a></li>
@@ -153,10 +153,10 @@ function getFooterContent(idx) {
             <div class="row">
                 <div class="col-lg-4 col-sm-6 ">
                     <div class="f_widget company_widget wow slideInLeft">
-                        <a href="https://www.palportals.com" class="f-logo"><img src="~~~images/1logo/logoWhite.svg" alt="logo"></a>
-                        <p class="mt_40">Copyright © 2020 <a href="https://www.palportals.com">PALPortals</a></p>
-                        <p class="mt_40"><a href="~~~legal.html" id="Legal">Legal</a> | <a href="~~~website-usage-terms-and-conditions.html" id="Terms">Terms of use</a> | <a href="~~~privacy-policy.html" id="Policy">Privacy</a> | <a href="~~~/subscribe/index.html" id="Subscribe">Subscribe us</a></p>
-                        <span style="color:#1c143b;">v.0.1.2020.12.28</span>                
+                        <a rel="canonical" href="https://www.palportals.com" class="f-logo"><img src="~~~images/1logo/logoWhite.svg" alt="logo"></a>
+                        <p class="mt_40">Copyright © 2020 <a rel="canonical" href="https://www.palportals.com">PALPortals</a></p>
+                        <p class="mt_40"><a rel="canonical" href="~~~legal" id="Legal">Legal</a> | <a rel="canonical" href="~~~website-usage-terms-and-conditions.html" id="Terms">Terms of use</a> | <a rel="canonical" href="~~~privacy-policy.html" id="Policy">Privacy</a> | <a rel="canonical" href="~~~/subscribe/index.html" id="Subscribe">Subscribe us</a></p>
+                        <span style="color:#1c143b;">v.0.1.2020.12.28</span>
                     </div>
                 </div>
                 <div class="col-lg-3 col-sm-6">
